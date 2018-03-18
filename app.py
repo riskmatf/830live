cd01a5ea3a9ef077b48cfad44c2f15ffb80c9687
Stevan <stevannestorovic@hotmail.com>
2018-03-18 13:27:58 +0100

    pocetna verzija, doraditi

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+
+array = ["Pera", "Mika", "Ana", "Janko"]
+
 @app.route("/")
 def index():
-	return "Hello world!"
-
+	return render_template("index.html")
 @app.route("/about")
 def about():
 	return "Flask demo app"
 
-@app.route("/hello/<string:name>")
-def hello(name):
+@app.route("/hello/<string:name>/<int:guess>")
+def hello(name, guess):
 	name =  name.capitalize()
-	return "Hello " + name + "!"
+	return render_template("hello.html", name=name, guess=guess) 
+
+@app.route("/users")
+def users():
+	return render_template("users.html", users=array)
+
+@app.route("/submit", methods=['GET'])
+def submit():
+	return render_template("submit.html")
+
+@app.route("/submit", methods=['POST'])
+def submit_post():
+	name = request.form.get("name")
+	link = request.form.get("link")
+	return "Uspesna prijava " + name + " link: " + link + " je postavljen."
+
+
+app.run()
